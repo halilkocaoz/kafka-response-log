938d5f69fb168d9ef13ff238442a56ebc3281238
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-03-19 02:37:20 +0300

    Add docker compose for zookeeper and kafka containers

--- server/NET/Kartaca.Intern/Startup.cs
+++ server/NET/Kartaca.Intern/Startup.cs
@@ -8,17 +8,12 @@ namespace Kartaca.Intern
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) 
-        {
-            services.AddControllers();
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddControllers();
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<TimeTrackerMiddleware>();
