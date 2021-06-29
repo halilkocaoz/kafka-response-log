aab75a3afeceb11d6887a6226bf42a0c57780aee
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-06-29 19:52:25 +0300

    Updates for db column changing. 	Simplifications

--- consumer/main.go
+++ consumer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 
@@ -10,16 +11,9 @@ import (
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
-var kafkaTopics = []string{"response_log"}
-var kafkaMessages [maxMessageCountToAccumulate]string
-var receivedMessageCount = 0
-
-var consumer *kafka.Consumer
-var db *sql.DB
-
 const (
 	maxMessageCountToAccumulate = 10
-	insertStatement             = `INSERT INTO net_logs (method, elapsed_time, timestamputc) VALUES ($1, $2, $3)`
+	insertStatement             = `INSERT INTO net_logs (method, elapsed_time, timestamp) VALUES ($1, $2, $3)`
 	database                    = "postgres"
 	databaseUser                = "postgres"
 	databasePass                = "psqlpass"
@@ -27,81 +21,44 @@ const (
 	kafkaServer                 = "kafka:9092"
 )
 
-func checkDatabase() {
-	fmt.Println("SQL Open")
-	var err error
-	db, err = sql.Open("postgres", "postgres://"+databaseUser+":"+databasePass+"@"+databaseServer+"/"+database+"?sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
+var (
+	kafkaTopics          = []string{"response_log"}
+	kafkaMessages        [maxMessageCountToAccumulate]string
+	receivedMessageCount = 0
+)
 
-	fmt.Println("SQL Open: OK")
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Database PING: OK")
-}
-
-func setDatabase() {
-	checkDatabase()
-
-	_, err := db.Exec(`create table if not exists net_logs
+func initDatabase(db *sql.DB) {
+	db.Exec(`create table if not exists net_logs
 	(
 		method varchar,
 		elapsed_time int,
-		timestamputc bigint
+		timestamp bigint
 	);`)
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
 }
 
-func createConsumer() {
-	consumerCreatingRepeatTime := 0
+func consumeAndInsertMessages(consumer *kafka.Consumer, db *sql.DB) error {
+	var (
+		err          error
+		kafkaMessage *kafka.Message
+	)
 
-consumerCreateStatement:
-	var err error
-	consumer, err = kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": kafkaServer,
-		"group.id":          "go-consumer",
-		"auto.offset.reset": "earliest",
-	})
-
-	if err != nil {
-		consumerCreatingRepeatTime++
-		if consumerCreatingRepeatTime <= 5 {
-			fmt.Println("Retrying to create consumer")
-			time.Sleep(3 * time.Second)
-			goto consumerCreateStatement
-		} else {
-			fmt.Printf("Tried %d times but", consumerCreatingRepeatTime)
-			panic(err)
-		}
-	}
-}
-
-func startConsuming() {
 	for {
-		kafkaMessage, err := consumer.ReadMessage(-100)
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			kafkaMessages[receivedMessageCount] = string(kafkaMessage.Value)
-			fmt.Println(kafkaMessages[receivedMessageCount])
-			receivedMessageCount++
+		kafkaMessage, err = consumer.ReadMessage(-1)
+		kafkaMessages[receivedMessageCount] = string(kafkaMessage.Value)
+		fmt.Println(kafkaMessages[receivedMessageCount])
+		receivedMessageCount++
 
-			if receivedMessageCount >= maxMessageCountToAccumulate {
-				commitMessagesDatabase()
-			}
+		if receivedMessageCount >= maxMessageCountToAccumulate {
+			err = insertMessages(db)
+		}
+
+		if err != nil {
+			return err
 		}
 	}
 }
 
-func commitMessagesDatabase() {
+func insertMessages(db *sql.DB) error {
 	dbTransaction, _ := db.Begin()
 
 	for i := 0; i < maxMessageCountToAccumulate; i++ {
@@ -109,36 +66,36 @@ func commitMessagesDatabase() {
 		dbTransaction.Exec(insertStatement,
 			splittedMsg[0], // method
 			splittedMsg[1], // elapsed time
-			splittedMsg[2], // utc timestamp
+			splittedMsg[2], // timestamp
 		)
 	}
 
-	err := dbTransaction.Commit()
-
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Printf("The last %d messages that received has been committed.\n", maxMessageCountToAccumulate)
-		receivedMessageCount = 0
-	}
+	receivedMessageCount = 0
+	return dbTransaction.Commit()
 }
 
 func main() {
-	time.Sleep(10 * time.Second)
-	setDatabase()
+	var (
+		consumer *kafka.Consumer
+	)
+
+	db, _ := sql.Open("postgres", "postgres://"+databaseUser+":"+databasePass+"@"+databaseServer+"/"+database+"?sslmode=disable")
+	initDatabase(db)
 	defer db.Close()
+	time.Sleep(10 * time.Second)
 
-	createConsumer()
+	consumer, _ = kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaServer,
+		"group.id":          "go-consumer",
+		"auto.offset.reset": "earliest",
+	})
+	consumer.SubscribeTopics(kafkaTopics, nil)
 
-subscribeStatement:
-	err := consumer.SubscribeTopics(kafkaTopics, nil)
+consuming:
+	err := consumeAndInsertMessages(consumer, db)
 
-	if err == nil {
-		fmt.Println("Consumer subscribed the topic")
-	} else {
-		fmt.Println(err.Error() + "\n Trying again")
-		goto subscribeStatement
+	if err != nil {
+		log.Fatal(err)
+		goto consuming
 	}
-
-	startConsuming()
 }
--- server/Controllers/HealthController.cs
+++ server/Controllers/HealthController.cs
@@ -9,26 +9,26 @@ using Npgsql;
 namespace Kafka.Example.Controllers
 {
     [ApiController]
-    [Route("[Controller]/api/products/")]
+    [Route("[Controller]/api/")]
     public class HealthController : ControllerBase
     {
         private readonly List<ResponseLog> logs = new List<ResponseLog>();
         private readonly NpgsqlConnection npgsqlConnection;
 
-        public HealthController(IConfiguration configuration) => 
-        npgsqlConnection = new NpgsqlConnection(configuration["Database:ConnectionString"]);
+        public HealthController(IConfiguration configuration) => npgsqlConnection = new NpgsqlConnection(configuration["Database:ConnectionString"]);
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [Route("products")]
+        [HttpGet()]
+        public async Task<IActionResult> ProductsHealth()
         {
             await npgsqlConnection.OpenAsync();
             var oneHourAgo = DateTimeOffset.UtcNow.AddHours(-1).ToUnixTimeSeconds();
 
-            await using (var cmd = new NpgsqlCommand($"select * from net_logs where timestamputc >= {oneHourAgo} order by timestamputc", npgsqlConnection))
+            await using (var cmd = new NpgsqlCommand($"select * from net_logs where timestamp >= {oneHourAgo} order by timestamp", npgsqlConnection))
             await using (var reader = await cmd.ExecuteReaderAsync()) while (await reader.ReadAsync())
-                {
-                    logs.Add(new ResponseLog(reader.GetString(0), reader.GetInt64(1), reader.GetInt64(2)));
-                }
+            {
+                logs.Add(new ResponseLog(reader.GetString(0), reader.GetInt64(1), reader.GetInt64(2)));
+            }
 
             #pragma warning disable CS4014
             npgsqlConnection.CloseAsync();
